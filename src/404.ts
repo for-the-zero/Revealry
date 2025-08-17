5894d953ed16ca1f4435902c8752ad8a9bcd0e76
for-the-zero <ourmicroid@outlook.com>
2025-08-17 22:29:36 +0800

    404 & favicon

---
+++ src/404.ts
@@ -0,0 +1,9 @@
+// components
+import 'mdui/components/button.js';
+// icons
+import '@mdui/icons/home--outlined.js';
+
+//
+import { init_i18n } from './public_assets/i18n';
+import config_static_404 from './_configs/404.static.yaml';
+init_i18n(config_static_404);
